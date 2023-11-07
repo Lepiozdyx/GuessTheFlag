c78a8985aaace1cd456db6f37f1d01d3fdde9689
Alex Zhulai <Lepiozdyx@gmail.com>
2023-11-07 18:13:47 -0300

    Do Challenge

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -16,6 +16,10 @@ struct ContentView: View {
     @State private var score = 0
     @State private var questionCount = 0
     
+    @State private var animationAmount = [0.0, 0.0, 0.0]
+    @State private var opacityAmount = [1.0, 1.0, 1.0]
+    @State private var scaleAmount = [1.0, 1.0, 1.0]
+    
     let maxQuestions = 8
     
     var body: some View {
@@ -24,7 +28,7 @@ struct ContentView: View {
                 .init(color: .cyan, location: 0.3),
                 .init(color: .mint, location: 0.3)
             ], center: .top, startRadius: 200, endRadius: 700)
-                .ignoresSafeArea()
+            .ignoresSafeArea()
             
             VStack {
                 Spacer()
@@ -44,11 +48,21 @@ struct ContentView: View {
                     
                     ForEach(0..<3) { number in
                         Button {
-                            flagTapped(number)
+                            withAnimation(.spring(duration: 0.6, bounce: 0.3)) {
+                                flagTapped(number)
+                                animationAmount[number] += 360
+                                
+                                for index in 0..<3 {
+                                    opacityAmount[index] = number == index ? 1 : 0.25
+                                    scaleAmount[index] = number == index ? 1 : 0.9
+                                }
+                            }
                         } label: {
                             FlagImage(countryName: countries[number])
                         }
-
+                        .opacity(opacityAmount[number])
+                        .scaleEffect(scaleAmount[number])
+                        .rotation3DEffect(.degrees(animationAmount[number]), axis: (x: 0, y: 1, z: 0))
                     }
                 }
                 .frame(maxWidth: .infinity)
@@ -99,8 +113,13 @@ struct ContentView: View {
     }
     
     private func askQuestion() {
-        countries.shuffle()
-        correctAnswer = Int.random(in: 0...2)
+        withAnimation {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+            animationAmount = [0, 0, 0]
+            opacityAmount = [1, 1, 1]
+            scaleAmount = [1.0, 1.0, 1.0]
+        }
     }
     
     private func reset() {
